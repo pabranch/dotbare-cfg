6907c902c2f8007d818f3285971cbb98f1e0d52c
Perry Branch <perryb@mailinator.com>
2026-04-24 21:07:23 -0600

    PS> Add UTF-8 configuration to PS profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,12 @@ Set-PSReadLineOption -Colors @{ Command = 'Cyan'; Operator = 'Yellow' }
 
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
+# UTF-8 for native command output and input
+if ([Console]::OutputEncoding.CodePage -ne 65001) { chcp 65001 > $null }
+[Console]::InputEncoding  = [System.Text.UTF8Encoding]::new($false)
+[Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+$OutputEncoding = [Console]::OutputEncoding
+
 function Upgrade-Apps-Elevated {
       gsudo powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "C:\Path\To\Upgrade-Apps.ps1"
 }
