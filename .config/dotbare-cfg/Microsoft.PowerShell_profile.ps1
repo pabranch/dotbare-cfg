494992687ef3e48b2795e9f2a544ef4fa9315ebc
Perry Branch <perryb@mailinator.com>
2026-08-07 14:55:47 -0600

    Remove unused PowerShell profile

--- .config/dotbare-cfg/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineOption -ViModeIndicator Cursor
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -Colors @{ Command = 'Cyan'; Operator = 'Yellow' }
-
-Set-PSReadLineKeyHandler -Key Tab -Function Complete
